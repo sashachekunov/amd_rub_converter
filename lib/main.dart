465c52aeb9b597895e045d6af774a533d0741ea6
Alexander Chekunov <achekunov@ab-technology.ru>
2022-12-31 03:49:19 +0400

    Add startup methods to App class

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,3 @@
-import 'package:amd_rub_converter/di/di.dart';
 import 'package:amd_rub_converter/presentation/app.dart';
 
-import 'package:flutter/material.dart';
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  await di.init();
-
-  runApp(const MyApp());
-}
+void main() => const AMDRUBConverter().initAndRun();
--- lib/presentation/app.dart
+++ lib/presentation/app.dart
@@ -1,20 +1,48 @@
 import 'package:amd_rub_converter/di/di.dart';
-import 'package:amd_rub_converter/presentation/bloc/converter/converter_cubit.dart';
 import 'package:amd_rub_converter/presentation/core/app_theme.dart';
+import 'package:amd_rub_converter/presentation/bloc/global_bloc_observer.dart';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
-
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter/foundation.dart' show kReleaseMode;
 import 'package:flutter/material.dart';
+import 'dart:async';
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+abstract class App extends StatelessWidget {
+  const App({super.key});
+
+  void initAndRun() async => await _init().whenComplete(_run);
+
+  Future<void> _init() async {
+    _ensureWidgetsInitialized();
+    _setupBlocObserver();
+    await _initAppComponents();
+  }
+
+  void _ensureWidgetsInitialized() => WidgetsFlutterBinding.ensureInitialized();
+
+  void _setupBlocObserver() => Bloc.observer = GlobalBlocObserver();
+
+  Future<void> _initAppComponents() async => await di.init();
+
+  void _run() => runZoned<void>(
+        () => runApp(this),
+        zoneSpecification: ZoneSpecification(print: _print),
+      );
+
+  void _print(Zone self, ZoneDelegate parent, Zone zone, String message) {
+    if (!kReleaseMode) parent.print(zone, "Intercepted: $message");
+  }
+}
+
+class AMDRUBConverter extends App {
+  const AMDRUBConverter({super.key});
 
   @override
-  Widget build(BuildContext context) => MultiBlocProvider(
-        providers: [
-          BlocProvider<ConverterCubit>(create: (_) => di.converterCubit),
-        ],
+  Widget build(BuildContext context) => BlocProvider(
+        create: (_) => di.converterCubit..initConverterState(),
         child: MaterialApp.router(
+          title: 'Конвертер ֏₽',
           debugShowCheckedModeBanner: false,
           supportedLocales: const [Locale('ru', 'RU')],
           locale: const Locale('ru', 'RU'),
