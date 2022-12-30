b85bbf31f94823175778455a9fa7627d4ed108c3
Alexander Chekunov <achekunov@ab-technology.ru>
2022-12-31 03:53:26 +0400

    dart format

--- test/data/data_sources/app_local_data_source.dart
+++ test/data/data_sources/app_local_data_source.dart
@@ -1,12 +1,15 @@
 import 'package:amd_rub_converter/data/core/db_client.dart';
 import 'package:amd_rub_converter/data/core/exceptions.dart';
 import 'package:amd_rub_converter/data/data_sources/app_local_data_source.dart';
+import 'package:amd_rub_converter/presentation/core/local_notifications.dart';
 
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:test/expect.dart';
 
 void testReadFirstLaunch(SharedPreferences preferences) async {
-  final dataSource = AppLocalDataSourceImpl(DBClient(preferences));
+  final dataSource = AppLocalDataSourceImpl(DBClient(preferences),
+      LocalNotifications(FlutterLocalNotificationsPlugin()));
 
   try {
     expect(await dataSource.readFirstLaunch(), true);
@@ -16,7 +19,8 @@ void testReadFirstLaunch(SharedPreferences preferences) async {
 }
 
 void testWriteFirstLaunch(SharedPreferences preferences) async {
-  final dataSource = AppLocalDataSourceImpl(DBClient(preferences));
+  final dataSource = AppLocalDataSourceImpl(DBClient(preferences),
+      LocalNotifications(FlutterLocalNotificationsPlugin()));
 
   try {
     await dataSource.writeFirstLaunch();
